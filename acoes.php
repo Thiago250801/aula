7f3f996e4faab61a87ecd9c95c8d2b4afa4a89f0
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-03 16:06:11 -0400

    adiciona verificação de sucesso ao criar usuário

--- acoes.php
+++ acoes.php
@@ -10,5 +10,14 @@ if (isset($_POST['create_usuario'])) {
 
     $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES ('$nome', '$email', '$data_nascimento', '$senha')";
     mysqli_query($conexao, $sql);
-} else {}
+
+    if (mysqli_affected_rows($conexao) > 0) {
+        $_SESSION['message'] = "Usuário criado com sucesso.";
+        header('Location: index.php');
+        exit;
+    } else { 
+        $_SESSION['message'] = "Erro ao criar usuário.";
+        header('Location: index.php');
+        exit;
+    }
 ?>
